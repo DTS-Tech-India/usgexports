75619e92992cb0c03905beaacacd223c26afd25e
kumarAmrendram <amrendram07@gmail.com>
2023-10-12 13:49:28 +0530

    - updates footer and new pages

---
+++ app/product/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <section className="relative z-10 pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
+                <h1>this is products page</h1>
+            </section>
+        </>
+    );
+};
+
+export default ErrorPage;
