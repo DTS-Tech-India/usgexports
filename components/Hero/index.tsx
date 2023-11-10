0258b7f2579b2b6ffd567b66893873992e2b4a67
Kumar Amrendram <kumaramrendram@gmail.com>
2023-11-10 20:12:13 +0530

    - uninstalls fonts

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -17,13 +17,13 @@ const Hero = () => {
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   Welcome to USG Exports
                 </h1>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                <p className="mb-12 text-base font-medium !leading-relaxed text-gray-500 dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                   At USG Exports, we take immense pride in connecting the world to the finest exports from India. With a legacy of excellence, we&apos;re your trusted partner in sourcing quality products that meet your exacting standards.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="/products"
-                    className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-primary/80"
+                    className="rounded-md bg-blue-700 py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Explore Our Products
                   </Link>
